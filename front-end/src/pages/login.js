55da23b1d2218a2bff3c1611f781d5599f85bbab
caiooliveirah09 <leelros@hotmail.com>
2023-01-23 19:17:52 -0300

    01login requisito 03 concluido

--- front-end/src/pages/login.js
+++ front-end/src/pages/login.js
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  const validateEmailAndPassword = () => {
+    const minLengthPassword = 6;
+    const emailRegex = /\S+@\S+\.\S+/;
+    return !(emailRegex.test(email) && password.length >= minLengthPassword);
+  };
+
+  useEffect(() => {
+    setIsDisabled(validateEmailAndPassword());
+  }, [email, password]);
+
   return (
     <div>
       <input
         data-testid="common_login__input-email"
         type="email"
         placeholder="email"
+        name="email"
+        value={ email }
+        onChange={ (e) => setEmail(e.target.value) }
       />
       <input
         data-testid="common_login__input-password"
         type="password"
+        name="password"
         placeholder="password"
+        value={ password }
+        onChange={ (e) => setPassword(e.target.value) }
       />
-      <button type="button" data-testid="common_login__button-login">
+      <button
+        type="button"
+        data-testid="common_login__button-login"
+        disabled={ isDisabled }
+      >
         Login
       </button>
       <button type="button" data-testid="common_login__button-register">
