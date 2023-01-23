20b8aa35e46b5326fd7f897ddbe206cbfa43c236
caiooliveirah09 <leelros@hotmail.com>
2023-01-23 18:54:31 -0300

    começando o 01login requisito 02 concluido

--- front-end/src/pages/login.js
+++ front-end/src/pages/login.js
@@ -3,7 +3,23 @@ import React from 'react';
 function Login() {
   return (
     <div>
-      oi
+      <input
+        data-testid="common_login__input-email"
+        type="email"
+        placeholder="email"
+      />
+      <input
+        data-testid="common_login__input-password"
+        type="password"
+        placeholder="password"
+      />
+      <button type="button" data-testid="common_login__button-login">
+        Login
+      </button>
+      <button type="button" data-testid="common_login__button-register">
+        Ainda não tenho conta
+      </button>
+      <p data-testid="common_login__element-invalid-email">aaaaa</p>
     </div>
   );
 }
