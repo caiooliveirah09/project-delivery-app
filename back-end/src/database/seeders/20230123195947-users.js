7d899e6fb7d4aa15ba9cc5d26e6f3080201ce7c4
caiooliveirah09 <leelros@hotmail.com>
2023-01-23 18:43:54 -0300

    começando o 01login requisito 1 concluido

--- back-end/src/database/seeders/20230123195947-users.js
+++ back-end/src/database/seeders/20230123195947-users.js
@@ -5,7 +5,7 @@ module.exports = {
     await queryInterface.bulkInsert('users', [
       {
         id: 1,
-        name: 'Delivery App Admin,',
+        name: 'Delivery App Admin',
         email: 'adm@deliveryapp.com',
         password: 'a4c86edecc5aee06eff8fdeda69e0d04',
         role: 'administrator',
