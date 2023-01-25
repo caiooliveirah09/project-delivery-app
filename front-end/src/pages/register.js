7b331e8396a156e69a761136480f65ac0cf0fef2
Marcelo Marques <marcelo-mls@hotmail.com>
2023-01-25 14:54:07 -0300

    02Register - Req 8

--- front-end/src/pages/register.js
+++ front-end/src/pages/register.js
@@ -1,6 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Register() {
+  const [userName, setUserName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isDisabled, setIsDisabled] = useState(true);
+  // const [isInvalid, setIsInvalid] = useState(false);
+  // const { setUserData } = useContext(appContext);
+  // const navigate = useNavigate();
+
+  const validateUserData = () => {
+    const minLengthPassword = 6;
+    const minLengthUserName = 12;
+    const emailRegex = /\S+@\S+\.\S+/;
+
+    return !(emailRegex.test(email)
+    && password.length >= minLengthPassword
+    && userName.length >= minLengthUserName);
+  };
+
+  // const register = async () => {
+  //   try {
+  //     const result = await api.post('/login', { email, password });
+  //     navigate('/register');
+  //   } catch (error) {
+  //     setIsInvalid(true);
+  //   }
+  // };
+
+  useEffect(() => {
+    setIsDisabled(validateUserData());
+  }, [userName, email, password]);
+
   return (
     <div>
       <input
@@ -8,29 +39,30 @@ function Register() {
         type="text"
         placeholder="name"
         name="name"
-        // value={ userName }
-        // onChange={ (e) => setEmail(e.target.value) }
+        value={ userName }
+        onChange={ (e) => setUserName(e.target.value) }
       />
       <input
         data-testid="common_register__input-email"
         type="email"
         placeholder="email"
         name="email"
-        // value={ email }
-        // onChange={ (e) => setEmail(e.target.value) }
+        value={ email }
+        onChange={ (e) => setEmail(e.target.value) }
       />
       <input
         data-testid="common_register__input-password"
         type="password"
         name="password"
         placeholder="password"
-        // value={ password }
-        // onChange={ (e) => setPassword(e.target.value) }
+        value={ password }
+        onChange={ (e) => setPassword(e.target.value) }
       />
       <button
         type="button"
         data-testid="common_register__button-register"
-        onClick={ () => navigate('/register') }
+        // onClick={ register }
+        disabled={ isDisabled }
       >
         Register
       </button>
