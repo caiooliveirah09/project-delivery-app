271e1bc6d32b2416a64ae3f0b699d1b509e4b4cb
caiooliveirah09 <leelros@hotmail.com>
2023-01-26 14:23:23 -0300

    03customerProduct requisito 15 e 16

--- front-end/src/components/card.js
+++ front-end/src/components/card.js
@@ -1,10 +1,39 @@
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 function Card(props) {
-  const { product } = props;
+  const { product, setExpense } = props;
   const { price, urlImage, name, id } = product;
+  const [quantity, setQuantity] = useState(0);
+
+  const saveItens = ({ target }) => {
+    const { value } = target;
+    setQuantity(Number(value));
+  };
+
+  const add = async () => {
+    setQuantity(quantity + 1);
+  };
+
+  const rm = () => {
+    if (quantity === 0) {
+      setQuantity(0);
+    } else {
+      setQuantity(quantity - 1);
+    }
+  };
+
+  useEffect(() => {
+    const cart = JSON.parse(localStorage.getItem('cart'));
+    cart[id - 1].quantity = Number(quantity);
+    localStorage.setItem('cart', JSON.stringify(cart));
+    const expense = cart
+      .reduce((acc, curr) => acc + (Number(curr.price) * Number(curr.quantity)), 0);
+    setExpense(expense);
+  }, [quantity]);
+
   return (
-    <div>
+    <div style={ { border: '2px solid red' } }>
       <p
         data-testid={ `customer_products__element-card-price-${id}` }
       >
@@ -22,18 +51,20 @@ function Card(props) {
       <button
         data-testid={ `customer_products__button-card-rm-item-${id}` }
         type="button"
+        onClick={ rm }
       >
         -
 
       </button>
       <input
         data-testid={ `customer_products__input-card-quantity-${id}` }
-        type="number"
-        value="0"
+        value={ quantity }
+        onChange={ saveItens }
       />
       <button
         data-testid={ `customer_products__button-card-add-item-${id}` }
         type="button"
+        onClick={ add }
       >
         +
 
--- front-end/src/pages/customerProducts.js
+++ front-end/src/pages/customerProducts.js
@@ -8,11 +8,12 @@ function CustomerProducts() {
   const [products, setProducts] = useState([]);
   const [apiIsLoaded, setApiIsLoaded] = useState(false);
   const [userData, setUserData] = useState(JSON.parse(localStorage.getItem('user')));
+  const [expense, setExpense] = useState(0);
+  const [isDisabled, setIsDisabled] = useState(true);
   const navigate = useNavigate();
-
   const logout = () => {
     setUserData({});
-    localStorage.clear();
+    localStorage.removeItem('user');
     navigate('/login');
   };
 
@@ -21,6 +22,8 @@ function CustomerProducts() {
       const result = await api.get('/products');
       setProducts(result.data);
       setApiIsLoaded(true);
+      const cart = result.data.map((product) => ({ ...product, quantity: 0 }));
+      localStorage.setItem('cart', JSON.stringify(cart));
     }
     getProducts();
   }, []);
@@ -38,20 +41,36 @@ function CustomerProducts() {
     }
   }, []);
 
+  useEffect(() => {
+    localStorage.setItem('expense', expense.toFixed(2).replace(/\./, ','));
+    if (expense > 0) {
+      setIsDisabled(false);
+    } else {
+      setIsDisabled(true);
+    }
+  }, [expense]);
+
   return (
     <div>
       { userData && <Nav userData={ userData } logout={ logout } />}
       <div>
         { apiIsLoaded && products.map((product) => (
-          <Card key={ product.id } product={ product } />
+          <Card key={ product.id } product={ product } setExpense={ setExpense } />
         ))}
       </div>
       <div>
         <button
           data-testid="customer_products__button-cart"
           type="button"
+          onClick={ () => navigate('/customer/checkout') }
+          disabled={ isDisabled }
         >
-          <p data-testid="customer_products__checkout-bottom-value">Ver Carrinho: R$ </p>
+          <span> Ver Carrinho: R$ </span>
+          <span
+            data-testid="customer_products__checkout-bottom-value"
+          >
+            { expense.toFixed(2).replace(/\./, ',') }
+          </span>
 
         </button>
       </div>
