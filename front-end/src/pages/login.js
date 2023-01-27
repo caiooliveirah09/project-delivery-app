4095ee3b28d5623d13ca4369bdef457258d09541
Marcelo Marques <marcelo-mls@hotmail.com>
2023-01-27 01:59:52 -0300

    Adiciona CSS do Login

--- front-end/src/pages/login.js
+++ front-end/src/pages/login.js
@@ -2,7 +2,8 @@ import React, { useEffect, useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import appContext from '../context/appContext';
 import api from '../services/api';
-import './login.css';
+import '../styles/login.css';
+import drinkSVG from '../images/drink.svg';
 
 function Login() {
   const [email, setEmail] = useState('');
@@ -40,41 +41,65 @@ function Login() {
   }, [email, password]);
 
   return (
-    <div className="login-container">
-      <div className="login-form">
-        <input
-          data-testid="common_login__input-email"
-          type="email"
-          placeholder="email"
-          name="email"
-          value={ email }
-          onChange={ (e) => setEmail(e.target.value) }
-        />
-        <input
-          data-testid="common_login__input-password"
-          type="password"
-          name="password"
-          placeholder="password"
-          value={ password }
-          onChange={ (e) => setPassword(e.target.value) }
-        />
-        <button
-          onClick={ login }
-          type="button"
-          data-testid="common_login__button-login"
-          disabled={ isDisabled }
-        >
-          Login
-        </button>
-        <button
-          type="button"
-          data-testid="common_login__button-register"
-          onClick={ () => navigate('/register') }
-        >
-          Register
-        </button>
+    <div className="login-page">
+      <div className="login-container">
+
+        <div className="login-logo">
+          <img src={ drinkSVG } alt="drink SVG" />
+          <div className="login-title">
+            <p className="title-drink">THE DRINK</p>
+            <p className="title-app">DELIVERY APP</p>
+          </div>
+        </div>
+
+        <div className="login-form">
+          <p className="login-form-p">Email:</p>
+          <input
+            className="login-form-input"
+            data-testid="common_login__input-email"
+            type="email"
+            placeholder="Type your email here"
+            name="email"
+            value={ email }
+            onChange={ (e) => setEmail(e.target.value) }
+          />
+          <p className="login-form-p">Password:</p>
+          <input
+            className="login-form-input"
+            data-testid="common_login__input-password"
+            type="password"
+            name="password"
+            placeholder="Do you know your password?"
+            value={ password }
+            onChange={ (e) => setPassword(e.target.value) }
+          />
+          <div className="login-form-buttons">
+            <button
+              className="login-form-btn login-form-btn-login"
+              onClick={ login }
+              type="button"
+              data-testid="common_login__button-login"
+              disabled={ isDisabled }
+            >
+              Login
+            </button>
+            <button
+              className="login-form-btn"
+              type="button"
+              data-testid="common_login__button-register"
+              onClick={ () => navigate('/register') }
+            >
+              Register
+            </button>
+          </div>
+        </div>
         {isInvalid && (
-          <p data-testid="common_login__element-invalid-email">Email invalido</p>
+          <p
+            data-testid="common_login__element-invalid-email"
+            className="login-invalid"
+          >
+            Ops! Invalid Credentials
+          </p>
         )}
       </div>
     </div>
