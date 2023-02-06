b7031fef1c87a1f48c93bc92d729341f1400a535
Elros <elros@pop-os.localdomain>
2023-02-06 15:26:17 -0300

    resolvi bugs

--- front-end/src/pages/sellerOrders.js
+++ front-end/src/pages/sellerOrders.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Nav from '../components/nav';
+import OrderCard from '../components/orderCard';
 import api from '../services/api';
 
 function SellerOrders() {
   const [sales, setSales] = useState([]);
   const [userData] = useState(JSON.parse(localStorage.getItem('user')));
+  const nine = 9;
 
   const navigate = useNavigate();
 
@@ -32,6 +34,15 @@ function SellerOrders() {
     getSales();
   }, []);
 
+  function setDate(date) {
+    const day = new Date(date).getUTCDate();
+    const month = new Date(date).getUTCMonth();
+    const year = new Date(date).getFullYear();
+    return (`${day < nine ? `0${day}` : day}`
+        + `/${month + 1 < nine ? `0${month + 1}` : month + 1}`
+        + `/${year}`);
+  }
+
   return (
     <>
       <Nav />
@@ -46,7 +57,7 @@ function SellerOrders() {
             >
               <p data-testid={ `${testIds.id}${sale.id}` }>{ sale.id }</p>
               <p data-testid={ `${testIds.status}${sale.id}` }>{sale.status}</p>
-              <p data-testid={ `${testIds.date}${sale.id}` }>{sale.saleDate}</p>
+              <p data-testid={ `${testIds.date}${sale.id}` }>{setDate(sale.saleDate)}</p>
               <p data-testid={ `${testIds.address}${sale.id}` }>
                 {`${sale.deliveryAddress}, ${sale.deliveryNumber}`}
               </p>
